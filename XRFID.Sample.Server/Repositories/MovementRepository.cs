41a527a05194648bfad86e08079d25c51b09c53f
Ruben Cesare Fasola <ruben.fasola@xerum.it>
2023-09-14 12:01:33 +0200

    feat: implemented feature to disable Movements

--- XRFID.Sample.Server/Repositories/MovementRepository.cs
+++ XRFID.Sample.Server/Repositories/MovementRepository.cs
@@ -8,4 +8,16 @@ public class MovementRepository : BaseRepository<Movement>
     public MovementRepository(XRFIDSampleContext context, ILogger<MovementRepository> logger) : base(context, logger)
     {
     }
+
+    public async Task<List<Movement>> DeactivateByReaderId(Guid readerId)
+    {
+        List<Movement> movments = await GetAsync(g => g.ReaderId == readerId);
+        List<Movement> updatedMovments = new List<Movement>();
+        foreach (Movement movment in movments)
+        {
+            movment.IsActive = false;
+            updatedMovments.Add(await UpdateAsync(movment));
+        }
+        return updatedMovments;
+    }
 }
