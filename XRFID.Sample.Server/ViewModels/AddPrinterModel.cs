6b234610e7918b0368f6d554e4c0cf65b3067ed5
Daniele Lucca <daniele.lucca@pernix.it>
2023-09-23 00:44:25 +0200

    imp: navigation and pages

--- XRFID.Sample.Server/ViewModels/AddPrinterModel.cs
+++ XRFID.Sample.Server/ViewModels/AddPrinterModel.cs
@@ -10,6 +10,7 @@ public class AddPrinterModel
     public string Code { get; set; }
     public string Reference { get; set; }
     [Required]
+    [RegularExpression(pattern: "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ErrorMessage = "Please enter a valid IPV4.")]
     public string Ip { get; set; }
     [Required]
     public int Port { get; set; } = 9100;
