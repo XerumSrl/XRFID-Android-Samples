6e814e8fbd374c0dd0d3a414b2031ae63969cd0e
Federico Corna <federico.corna@xerum.it>
2023-09-22 14:45:48 +0200

    feat: edit & delete loading unit

--- XRFID.Sample.Server/ViewModels/AddProductModel.cs
+++ XRFID.Sample.Server/ViewModels/AddProductModel.cs
@@ -8,6 +8,9 @@ public class AddProductModel
     public string Name { get; set; } = string.Empty;
 
     [Required]
+    public string Code { get; set; } = string.Empty;
+
+    [Required]
     public string Description { get; set; } = string.Empty;
 
     public string? Note { get; set; }
